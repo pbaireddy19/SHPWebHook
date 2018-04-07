402a72f2bf69ab2c2dae73c078ed481dcf0e13ba
prasanth Baireddy <pbaireddy19@gmail.com>
2018-04-06 17:26:17 -0700

    updated google assistant response

--- main.go
+++ main.go
@@ -20,11 +20,23 @@ func main() {
 func getDeviceDetails(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("Received request to get device details")
+	var request WebHookRequest 
+	_ = json.NewDecoder(r.Body).Decode(&request)
 	body, err := ioutil.ReadAll(r.Body)
 	fmt.Println(string(body))
 	fmt.Println(err)
-	speech := "There are 5 devices online"
-	displayText:= "There are 5 devices online"
+	var speech = ""
+	var displayText = ""
+	
+	intentName := request.Result.IntentName
+	
+	if intentName == "network-summary-intent" {
+		speech = "Okay, there are 5 online devices on your network. 1 device is currently blocked. We are trying to determine the status of McAfee Antivirus protection for your devices. You also have unread notifications. Would you like me to read out a few?"
+		displayText = "Okay, there are 5 online devices on your network. 1 device is currently blocked. We are trying to determine the status of McAfee Antivirus protection for your devices. You also have unread notifications. Would you like me to read out a few?"
+	} else if intentName == "devices-intent" {
+		speech = "I can help you with that.There are 5 devices online"
+		displayText = "I can help you with that.There are 5 devices online"
+	}
 
 	hookResp := WebHookResp {
 		speech,
